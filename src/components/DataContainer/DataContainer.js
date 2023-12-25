c50c7932fc731d842e7e46d0fc13d5dc2142998e
Anais Weber <anaisweber@mbp-de-anais.home>
2023-12-25 13:01:17 +0100

    css update

--- src/components/DataContainer/DataContainer.js
+++ src/components/DataContainer/DataContainer.js
@@ -14,17 +14,17 @@ import "./dataContainer.css";
 const DataContainer = ({ theme }) => {
   const [fetchCodesNafData, setFetchCodesNafData] = useState(null);
   const [idccData, setIdccData] = useState(null);
+  const [showAdditionalInfo, setShowAdditionalInfo] = useState(Array(0).fill(false));
 
   const className = theme === "bg-dark" ? "-dark" : "-light";
 
   const { data, isLoading, setSelectedItem } = useData();
-  const [showAdditionalInfo, setShowAdditionalInfo] = useState(false);
 
-  const toggleAdditionalInfo = () => {
-      setShowAdditionalInfo(!showAdditionalInfo);
-    };
-
-
+  const toggleAdditionalInfo = (index) => {
+    const newShowAdditionalInfo = [...showAdditionalInfo];
+    newShowAdditionalInfo[index] = !newShowAdditionalInfo[index];
+    setShowAdditionalInfo(newShowAdditionalInfo);
+  };
 
   const handleSelectedItem = (item) => {
     setSelectedItem(item);
@@ -78,114 +78,112 @@ const DataContainer = ({ theme }) => {
     return (
       <div className={`data-container${className}`}>
         <div className={`cards-container${className}`}>
-          {data[0]?.map((item) => (
+          {data[0]?.map((item, index) => (
             <div className={`card${className}`} key={item.id}>
               <div className={`card-content${className}`}>
-               
-                  <Link
-                    to={`/Details/${item.id}`}
-                    className="card-btn-info"
-                    onClick={() => {
-                      handleSelectedItem(item);
-                    }}
-                    style={{ textDecoration: 'none' }}
-                  >
-                     <div className="card-top"></div>
-                    <div className="card-title">
-                      {item.nom_complet} ({item.nom_raison_sociale})
-                    </div>
-                    <div className="more-infos">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
-                        <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
-                        <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
-                      </svg>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                      </svg>
-                    </div>
-                  </Link>
-                  <div className="btn-infos-display" onClick={toggleAdditionalInfo}>
-                  {showAdditionalInfo ? "Masquer" : "Afficher plus"}
-                </div>
-                {showAdditionalInfo && (
-                <div className="card-content-bottom">
-                <div className="map">
-                  <DataMap 
-                    latitude={item.siege.latitude} 
-                    longitude={item.siege.longitude}
-                    name={item.nom_complet}
-                    address={item.siege.geo_adresse} />
-                </div>
-                <div className="main-infos">
-                  <span>
-                    {item.siege.geo_adresse ? item.siege.geo_adresse : item.siege.adresse.toLowerCase()}
-                  </span>
-                  <span>
-                    {getNafLabel(item.activite_principale)}
-                  </span>
-                  <span>
-                    {trancheEffectifData[item.tranche_effectif_salarie]}
-                  </span>
-                </div>
-                <div className="link-container">
-                  <div className="link-egapro">
-                    {item.complements.egapro_renseignee === true ? (
-                      <a href={`https://egapro.travail.gouv.fr/index-egapro/recherche?query=${item.siren}`} target="_blank" rel="noopener noreferrer" className="egapro-url">
-                        <div className="link-egapro-item">
-                          <div className="text">
-                            Index d'égalité professionnelle
-                          </div>
-                          <div>
-                            <FontAwesomeIcon icon={faUpRightFromSquare} size="xs" className="icon" />
-                          </div>
-                        </div>
-                      </a>
-                    ) : (
-                      null
-                    )}
+                <Link
+                  to={`/Details/${item.id}`}
+                  className="card-btn-info"
+                  onClick={() => {
+                    handleSelectedItem(item);
+                  }}
+                  style={{ textDecoration: 'none' }}
+                >
+                  <div className="card-top"></div>
+                  <div className="card-title">
+                    {item.nom_complet} ({item.nom_raison_sociale})
                   </div>
-
-                  {idccData && (
-                    <div className="conventions-container"> 
-                      {idccData
-                        .filter((idccItem) => idccItem.siret === item.siege.siret)
-                        .map((matchedIdccItem) => (
-                          <div key={`${item.id}-${matchedIdccItem.siret}`}>
-                            {matchedIdccItem.idccData &&
-                              matchedIdccItem.idccData.map((convention) => (
-                                <div key={convention.id}>
-                                  {convention.conventions && convention.conventions.length > 0 ? (
-                                    <>
-                                      <div className="idcc-header">
-                                        Conventions collectives applicables
-                                      </div>
-                                      <div className="idcc-map">
-                                        {convention.conventions.map((singleConvention) => (
-                                          <div key={singleConvention.id}>
-                                            <a href={singleConvention.url} target="_blank" rel="noopener noreferrer" className="link-url">
-                                              <div className="link">
-                                                <div>{singleConvention.shortTitle}</div>
-                                                <FontAwesomeIcon icon={faUpRightFromSquare} size="xs" className="icon" />
-                                              </div>
-                                            </a>
-                                          </div>
-                                        ))}
-                                      </div>
-                                    </>
-                                  ) : (
-                                    null
-                                  )}
-                                </div>
-                              ))}
-                          </div>
-                        ))}
-                    </div>
-                  )}
+                  <div className="more-infos">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark-text" viewBox="0 0 16 16">
+                      <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
+                      <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                    </svg>
+                  </div>
+                </Link>
+                <div className="btn-infos-display" onClick={() => toggleAdditionalInfo(index)}>
+                  {showAdditionalInfo[index] ? "Masquer" : "Afficher plus"}
                 </div>
-              </div>
-              )}
-             
+                {showAdditionalInfo[index] && (
+                  <div className="card-content-bottom">
+                    <div className="map">
+                      <DataMap 
+                        latitude={item.siege.latitude} 
+                        longitude={item.siege.longitude}
+                        name={item.nom_complet}
+                        address={item.siege.geo_adresse} />
+                    </div>
+                    <div className="main-infos">
+                      <span>
+                        {item.siege.geo_adresse ? item.siege.geo_adresse : item.siege.adresse.toLowerCase()}
+                      </span>
+                      <span>
+                        {getNafLabel(item.activite_principale)}
+                      </span>
+                      <span>
+                        {trancheEffectifData[item.tranche_effectif_salarie]}
+                      </span>
+                    </div>
+                    <div className="link-container">
+                      <div className="link-egapro">
+                        {item.complements.egapro_renseignee === true ? (
+                          <a href={`https://egapro.travail.gouv.fr/index-egapro/recherche?query=${item.siren}`} target="_blank" rel="noopener noreferrer" className="egapro-url">
+                            <div className="link-egapro-item">
+                              <div className="text">
+                                Index d'égalité professionnelle
+                              </div>
+                              <div>
+                                <FontAwesomeIcon icon={faUpRightFromSquare} size="xs" className="icon" />
+                              </div>
+                            </div>
+                          </a>
+                        ) : (
+                          null
+                        )}
+                      </div>
+
+                      {idccData && (
+                        <div className="conventions-container"> 
+                          {idccData
+                            .filter((idccItem) => idccItem.siret === item.siege.siret)
+                            .map((matchedIdccItem) => (
+                              <div key={`${item.id}-${matchedIdccItem.siret}`}>
+                                {matchedIdccItem.idccData &&
+                                  matchedIdccItem.idccData.map((convention) => (
+                                    <div key={convention.id}>
+                                      {convention.conventions && convention.conventions.length > 0 ? (
+                                        <>
+                                          <div className="idcc-header">
+                                            Conventions collectives applicables
+                                          </div>
+                                          <div className="idcc-map">
+                                            {convention.conventions.map((singleConvention) => (
+                                              <div key={singleConvention.id}>
+                                                <a href={singleConvention.url} target="_blank" rel="noopener noreferrer" className="link-url">
+                                                  <div className="link">
+                                                    <div>{singleConvention.shortTitle}</div>
+                                                    <FontAwesomeIcon icon={faUpRightFromSquare} size="xs" className="icon" />
+                                                  </div>
+                                                </a>
+                                              </div>
+                                            ))}
+                                          </div>
+                                        </>
+                                      ) : (
+                                        null
+                                      )}
+                                    </div>
+                                  ))}
+                              </div>
+                            ))}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           ))}
--- src/components/Details/EtablissementsDisplay.js
+++ src/components/Details/EtablissementsDisplay.js
@@ -9,16 +9,14 @@ import { trancheEffectifData } from "../../functions/ExportDefinitions";
 import "./etablissementsDisplay.css";
 
 const EtablissementsDisplay = ({
-  selectedDataItem,
   theme,
   currentEtablissements,
-
 }) => {
   const [fetchCodesNafData, setFetchCodesNafData] = useState(null);
   const [idccDataList, setIdccDataList] = useState([]);
+  const [showDetails, setShowDetails] = useState(Array(currentEtablissements.length).fill(false));
+
   const className = theme === "bg-dark" ? "-dark" : "-light";
-
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -55,89 +53,103 @@ const EtablissementsDisplay = ({
     fetchAllIdccData();
   }, [currentEtablissements]);
 
-
-
-  
+  const toggleDetails = (index) => {
+    const newShowDetails = [...showDetails];
+    newShowDetails[index] = !newShowDetails[index];
+    setShowDetails(newShowDetails);
+  };
 
   return (
     <div className={`etablissements-container${className}`}>
-
-      <p className="etablissements-count">
-        {selectedDataItem.nombre_etablissements_ouverts} établissement(s)
-      </p>
+    <div className="etablissements-title">
+      Etablissements répértoriés
+    </div>
       <div className="etablissements-list">
         <div className="etablissements-scroll">
-        <div className="etablissement-cards">
-          
-          {currentEtablissements.map((etablissement, index) => {
-            const idccData = idccDataList[index] || [];
-            // setCurrentAddress(etablissement.adresse);
+          <div className="etablissement-cards">
+            {currentEtablissements.map((etablissement, index) => {
+              const idccData = idccDataList[index] || [];
 
-            return (
-              <div key={index} className={`etablissement-card${className}`}>
-                <div className="left-container">
-                  <div className="entreprise-info">
-                    <div className="etablissement-header">
-                      <h2>{etablissement.est_siege ? "Siège" : "Établissement de"}</h2>
-                      <p>{etablissement.libelle_commune}</p>
-                    </div>
-                    <div className="voir-plus">voir plus</div>
-                    <div className={`etablissement-details${className}`}>
-                    <div className="right-container">
-                <div className="map-container">
-                    <div className="map">
-                      <EtablissementMap
-                      
-                        latitude={etablissement.latitude}
-                        longitude={etablissement.longitude}
-                        address={etablissement.adresse}
-                    
-                      />
-                    </div>
-                  </div>
+              return (
+                <div key={index} className={`etablissement-card${className}`}>
 
-                </div>
-                      <p>{etablissement.adresse.toLowerCase()}</p>
-                      <p>{getNafLabel(etablissement.activite_principale)}</p>
-                      {etablissement.tranche_effectif_salarie !== null && etablissement.tranche_effectif_salarie !== undefined && (
-                      <p>{trancheEffectifData[etablissement.tranche_effectif_salarie]}</p>)}
-
-                      <a
-                        className="legi-link"
-                        href={`https://www.legifrance.gouv.fr/liste/acco?siret=${etablissement.siret}&sortValue=DATE_PUBLI_DESC&pageSize=10&page=1&tab_selection=all#acco`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <div className="link-text">consulter les accords d'établissement</div>
-                        <FontAwesomeIcon icon={faUpRightFromSquare} size="xs" className="link-icon" />
-                      </a>
-
-                      <div className="idcc-card-container">
-                        {idccData.map((item, idccIndex) => (
-                          <div key={idccIndex} className="idcc-card-container">
-                            {item.conventions.map((convention, conventionIndex) => (
-                              <div key={conventionIndex}>
-                                <a href={convention.url} 
-                                target="_blank" 
-                                rel="noopener noreferrer"
-                                className="idcc-legi-link">
-                                  {convention.shortTitle}
-                                  <FontAwesomeIcon icon={faUpRightFromSquare} size="xs" className="link-icon" />
-                                </a>
-                              </div>
-                            ))}
-                          </div>
-                        ))}
+                  <div className="etablissement-header">
+                        <h2>{etablissement.est_siege ? "Siège" : "Établissement de"}</h2>
+                        <p>{etablissement.libelle_commune}</p>
                       </div>
+                  <div className="left-container">
+                    <div className="entreprise-info">
+                      <div
+                        className="see-more"
+                        onClick={() => toggleDetails(index)}
+                      >
+                        Afficher plus
+                      </div>
+                      {showDetails[index] && (
+                        <div className={`etablissement-details${className}`}>
+                          <div className="right-container">
+                            <div className="map-container">
+                              <div className="map">
+                                <EtablissementMap
+                                  latitude={etablissement.latitude}
+                                  longitude={etablissement.longitude}
+                                  address={etablissement.adresse}
+                                />
+                              </div>
+                            </div>
+                            <p>{etablissement.adresse.toLowerCase()}</p>
+                            <p>{getNafLabel(etablissement.activite_principale)}</p>
+                            {etablissement.tranche_effectif_salarie !== null &&
+                            etablissement.tranche_effectif_salarie !== undefined && (
+                              <p>{trancheEffectifData[etablissement.tranche_effectif_salarie]}</p>
+                            )}
+                            <a
+                              className="legi-link"
+                              href={`https://www.legifrance.gouv.fr/liste/acco?siret=${etablissement.siret}&sortValue=DATE_PUBLI_DESC&pageSize=10&page=1&tab_selection=all#acco`}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <div className="link-text">
+                                Consulter les accords d'établissement
+                              </div>
+                              <FontAwesomeIcon
+                                icon={faUpRightFromSquare}
+                                size="xs"
+                                className="link-icon"
+                              />
+                            </a>
+                            <div className="idcc-card-container">
+                              {idccData.map((item, idccIndex) => (
+                                <div key={idccIndex} className="idcc-card-container">
+                                  {item.conventions.map((convention, conventionIndex) => (
+                                    <div key={conventionIndex}>
+                                      <a
+                                        href={convention.url}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="idcc-legi-link"
+                                      >
+                                        {convention.shortTitle}
+                                        <FontAwesomeIcon
+                                          icon={faUpRightFromSquare}
+                                          size="xs"
+                                          className="link-icon"
+                                        />
+                                      </a>
+                                    </div>
+                                  ))}
+                                </div>
+                              ))}
+                            </div>
+                          </div>
+                        </div>
+                      )}
                     </div>
                   </div>
                 </div>
-
-              </div>
-            );
-          })}
-        </div>
-
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
